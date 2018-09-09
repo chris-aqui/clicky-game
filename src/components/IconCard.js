f022b5e4286f41608b6698cfb36a8bbb1ac0a1f9
Chris <aqui.christine@gmail.com>
2018-09-09 10:39:11 -0400

    added some layout

--- src/components/IconCard.js
+++ src/components/IconCard.js
@@ -6,7 +6,7 @@ class IconCard extends Component {
   render(){
     return (
       <section className="tc pa3">
-        <article className="w5 pa2 bb b--black-10">
+        <article className="w4 pa2 ba bb b--black-10">
           <img src={this.props.image} className="db" alt="Photo" />
         </article>
       </section>
